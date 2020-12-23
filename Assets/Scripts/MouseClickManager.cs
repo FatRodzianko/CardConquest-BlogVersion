15e4bc44b70e1211ae852dca8efb8d5ef86ab8b7
@FatRodzianko <jmurphy.5277@gmail.com>
2020-12-23 12:17:16 -0600

    post 2

--- Assets/Scripts/MouseClickManager.cs
+++ Assets/Scripts/MouseClickManager.cs
@@ -6,11 +6,12 @@ public class MouseClickManager : MonoBehaviour
 {
     [SerializeField]
     private LayerMask unitLayer;
+    public List<GameObject> unitsSelected;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        unitsSelected = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -24,12 +25,44 @@ public class MouseClickManager : MonoBehaviour
 
             if (rayHitUnit.collider != null)
             {
-                Debug.Log("clicked on unit object.");
+                UnitScript unitScript = rayHitUnit.collider.GetComponent<UnitScript>();
+                if (!unitScript.currentlySelected)
+                {
+                    Debug.Log("Selecting a new unit.");
+                    unitsSelected.Add(rayHitUnit.collider.gameObject);
+                    unitScript.currentlySelected = !unitScript.currentlySelected;
+                    unitScript.ClickedOn();
+                }
+                else 
+                {
+                    unitsSelected.Remove(rayHitUnit.collider.gameObject);
+                    Debug.Log("Deselecting the unit unit.");
+                    unitScript.currentlySelected = !unitScript.currentlySelected;
+                    unitScript.ClickedOn();
+                }
             }
             else
             {
-                Debug.Log("did not click on unit object.");
+                
             }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right clicked.");
+            ClearUnitSelection();
+        }
+    }
+    void ClearUnitSelection()
+    {
+        if (unitsSelected.Count > 0)
+        {
+            foreach (GameObject unit in unitsSelected)
+            {
+                UnitScript unitScript = unit.GetComponent<UnitScript>();
+                unitScript.currentlySelected = !unitScript.currentlySelected;
+                unitScript.ClickedOn();
+            }
+            unitsSelected.Clear();
         }
     }
 }
---
+++ Assets/Scripts/UnitScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitScript : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject outline;
+    public bool currentlySelected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+		outline = Instantiate(outline, transform.position, Quaternion.identity);
+		outline.transform.SetParent(gameObject.transform);
+		ClickedOn();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void ClickedOn()
+	{
+		if (currentlySelected)
+		{
+			outline.SetActive(true);
+			Debug.Log("Currently selected set to true. Activating outline.");
+			outline.transform.position = transform.position;
+
+		}
+		else if (!currentlySelected)
+		{
+			if (outline.activeInHierarchy)
+			{
+				Debug.Log("Currently selected set to false. Deactivating outline");
+				outline.SetActive(false);
+			}
+		}
+	}
+}
