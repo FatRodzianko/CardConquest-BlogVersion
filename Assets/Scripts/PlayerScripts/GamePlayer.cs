1e5cb686f147a378d144f72726076ba6a0a27937
@FatRodzianko <jmurphy.5277@gmail.com>
2021-02-10 16:20:54 -0600

    post-14

---
+++ Assets/Scripts/PlayerScripts/GamePlayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+using System.Linq;
+
+public class GamePlayer : NetworkBehaviour
+{
+    [SyncVar] public string PlayerName;
+    [SyncVar] public int ConnectionId;
+
+    private NetworkManagerCC game;
+    private NetworkManagerCC Game
+    {
+        get
+        {
+            if (game != null)
+            {
+                return game;
+            }
+            return game = NetworkManagerCC.singleton as NetworkManagerCC;
+        }
+    }
+    public override void OnStartAuthority()
+    {
+        gameObject.name = "LocalGamePlayer";
+        Debug.Log("Labeling the local player: " + this.PlayerName);
+    }
+    public override void OnStartClient()
+    {
+        DontDestroyOnLoad(gameObject);
+        Game.GamePlayers.Add(this);
+        Debug.Log("Added to GamePlayer list: " + this.PlayerName);
+    }
+    public override void OnStopClient()
+    {
+        Debug.Log(PlayerName + " is quiting the game.");
+        Game.GamePlayers.Remove(this);
+        Debug.Log("Removed player from the GamePlayer list: " + this.PlayerName);
+    }
+    [Server]
+    public void SetPlayerName(string playerName)
+    {
+        this.PlayerName = playerName;
+    }
+    [Server]
+    public void SetConnectionId(int connId)
+    {
+        this.ConnectionId = connId;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
